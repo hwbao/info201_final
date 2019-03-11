78b33c63cc31a702b18b743d557475baf6eb2862
bravolistic <bravolistic16@gmail.com>
2019-03-10 17:44:37 -0700

    css

--- scripts/backer.R
+++ scripts/backer.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(plotly)
 
 # Read the data about kickstarter and convert it into a dataframe
-kickstarter <- read.csv("data/ks_projects_201801.csv")
+kickstarter <- read.csv("./data/ks_projects_201801.csv")
 data.frame(kickstarter)
 
 project_name <- "Art"
