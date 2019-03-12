49151324150893e0c53f0ac5de4223e5d1749578
bravolistic <bravolistic16@gmail.com>
2019-03-11 19:14:39 -0700

    local chart3 changes

--- chart#3.R
+++ chart#3.R
@@ -7,29 +7,29 @@ find_rate <- function(data, input_category, input_year){
   modified_df <- data %>% 
     select(input_category, state, deadline) %>% 
     mutate(deadline = substring(deadline, 1, 4)) %>% 
-    group_by_(input_category) %>% 
+    group_by_("input_category") %>% 
     filter(deadline == input_year) %>% 
     mutate(total_count = n()) %>% 
     filter(state == "successful") %>%
     mutate(success = n()) %>% 
     mutate(success_rate = round(success / total_count * 100, 1)) %>% 
-    select(input_category, success_rate) %>% 
-    unique(by = input_category) %>% 
+    select("input_category", success_rate) %>% 
+    unique(by = "input_category") %>% 
     arrange(success_rate)
   
-  modified_df$input_category <- factor(modified_df$input_category, 
-                          levels = c(as.character(modified_df$input_category)))
+  modified_df$"input_category" <- factor(modified_df$"input_category", 
+                          levels = c(as.character(modified_df$"input_category")))
   
   p <- plot_ly(
     modified_df,
-    x = ~input_category,
+    x = "input_category",
     y = ~success_rate,
     name = "Race chart",
     type = "bar"
     #marker = list(color = input$colors)
   ) %>%
     layout(
-      title = paste("Success Rate vs. Different Main Category"),
+      title = paste("Success Rate vs. Different Main Category in ", input_year),
       yaxis = list(title = "Success Rate (%)"),
       xaxis = list(title = "Category")
     )
