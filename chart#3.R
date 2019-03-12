f63758d70f3365d994ccc2571e217d93dfb78d5f
Yutian Lei <leiy4@uw.edu>
2019-03-11 17:20:34 -0700

    add line plot

--- chart#3.R
+++ chart#3.R
@@ -1,25 +1,14 @@
 library("shiny")
 library("plotly")
 library("dplyr")
-library("lubridate")
 library(collection)
 
-ks_data <- read.csv("data/ks-projects-201801.csv", stringsAsFactors = F)
-
-category_data2 <- ks_data %>% 
-  select(category, main_category, state, deadline) %>% 
-  filter(main_category == "Art") %>% 
-  group_by(category) %>% 
-  summarise(count = n())
-
-category_data <- ks_data %>% 
-  select(category, main_category, state, deadline)
-  #format(as.Date(category_data$deadline, format="%Y-%m-%d"), "%Y")
+ks_data <- read.csv("./data/ks_projects_201801.csv", stringsAsFactors = F)
 
 find_rate <- function(data, input_category, input_year){
   modified_df <- data %>% 
     select(input_category, state, deadline) %>% 
-    mutate(deadline = substring(deadline, 1, 4)) %>% 
+    mutate(deadline = as.numeric(substring(deadline, 1, 4))) %>% 
     group_by_(input_category) %>% 
     filter(deadline == input_year) %>% 
     mutate(total_count = n()) %>% 
@@ -27,13 +16,32 @@ find_rate <- function(data, input_category, input_year){
     mutate(success = n()) %>% 
     mutate(success_rate = round(success / total_count * 100, 1)) %>% 
     select(input_category, success_rate) %>% 
-    unique(by = input_category)
+    unique(by = input_category) %>% 
+    arrange(success_rate)
 }
 
-test_df <- find_rate(ks_data, "main_category", "2016")
+test_df <- find_rate(ks_data, "main_category", 2016)
 
-p <- plot_ly(
-  main_cat,
+find_line <- function(data, sub_category){
+  modified_df <- data %>% 
+    select(category, state, deadline) %>% 
+    mutate(deadline = as.numeric(substring(deadline, 1, 4))) %>% 
+    group_by(deadline) %>% 
+    filter(category == sub_category) %>% 
+    mutate(total_count = n()) %>% 
+    filter(state == "successful") %>%
+    mutate(success = n()) %>% 
+    mutate(success_rate = round(success / total_count * 100, 1)) %>% 
+    select(category, success_rate, deadline) %>% 
+    unique(by = deadline) %>% 
+    ungroup(deadline) %>% 
+    arrange(deadline)
+}
+
+test_line <- find_line(ks_data, "Drinks")
+
+p1 <- plot_ly(
+  test_df,
   x = ~main_category,
   y = ~success_rate,
   name = "Race chart",
@@ -45,8 +53,15 @@ p <- plot_ly(
     yaxis = list(title = "Population (thousand)"),
     xaxis = list(title = "Race")
   )
-p
+p1
 
+p2 <- plot_ly(
+  test_line, 
+  x = ~deadline, 
+  y = ~success_rate, 
+  type = 'scatter', 
+  mode = 'lines+markers')
+p2
 
 
 
