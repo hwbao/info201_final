b33dbd55953702008f720056517337af1978541e
Celia <celiazinc>
2019-03-12 16:35:41 -0700

    compelete total_backer

--- total_backer.R
+++ total_backer.R
@@ -5,14 +5,23 @@ library(plotly)
 # Read the data about kickstarter and convert it into a dataframe
 kickstarter <- read.csv("data/ks_projects_201801.csv", stringsAsFactors = F)
 
+top <- "20"
+
+draw_sankey_graph <- function(kickstarter, top) {
 new_data <- kickstarter %>%
-  dplyr::group_by(category, main_category) %>%
+  dplyr::filter(main_category != category) %>%
+  dplyr::group_by(main_category, category) %>%
   dplyr::summarize(category_pledged = sum(usd_pledged_real)) %>% 
-  ungroup() %>% 
-  top_n(20, wt=category_pledged)
+  ungroup() %>%
+  top_n(as.numeric(top), wt=category_pledged)
 
 new_data$fac_main_cate <- as.numeric(factor(new_data$main_category)) - 1
-new_data$fac_cate <- as.numeric(factor(new_data$category)) + 7
+
+arrange_data <- new_data %>%
+  arrange(fac_main_cate)
+
+arrange_data$fac_cate <- as.numeric(factor(arrange_data$category)) + 6
+
   
   draw_graph <- plot_ly(type = "sankey", 
                         domain = list(x =  c(0,1), y =  c(0,1)),
@@ -21,25 +30,31 @@ new_data$fac_cate <- as.numeric(factor(new_data$category)) + 7
                         node = list(
                           # label = c(new_data$fac_main_cate, new_data$fac_cate),
                           # label = 0:34, # works!
-                          label = c(unique(new_data$main_category), unique(new_data$category)),
-                          
-                          color = "#262C46",
+                          label = c(unique(arrange_data$main_category), sort(unique(arrange_data$category), decreasing = F)),
+                          color = "#D175B7",
                           pad = 15,
-                          thickness = 15,
+                          thickness = 20,
                           line = list(color = "black", width = 0.5)
                         ),
                         
-                        link = list(source = new_data$fac_main_cate,
-                                    target = new_data$fac_cate,
-                                    value =  new_data$category_pledged)
+                        link = list(source = arrange_data$fac_main_cate,
+                                    target = arrange_data$fac_cate,
+                                    value =  arrange_data$category_pledged,
+                                    color = "#4BC3B5")
                       ) %>%
     layout(
       title = "Total pledged each category get and distribution to their sub category",
       font = list(
-        size = 11
+        size = 11,
+        color = "white"
       ),
-      xaxis = list(showgrid = F, zeroline = F),
-      yaxis = list(showgrid = F, zeroline = F)
+      xaxis = list(showgrid = T, zeroline = T),
+      yaxis = list(showgrid = F, zeroline = F),
+      plot_bgcolor = "#1C2C54",
+      paper_bgcolor = "#1C2C54"
     )
 
-  draw_graph
+  return(draw_graph)
+}
+  
+  
