9c3e7cf97fafba17008e34fb9c78f2f925c105ca
Yutian Lei <leiy4@uw.edu>
2019-03-14 02:56:20 -0700

    adjust

--- chart#3.R
+++ chart#3.R
@@ -112,56 +112,35 @@ line_plot <- function(data, input_sub_cate) {
     ungroup(deadline) %>%
     arrange(deadline)
   
-  accumulate_by <- function(dat, var) {
-    var <- lazyeval::f_eval(var, dat)
-    lvls <- plotly:::getLevels(var)
-    dats <- lapply(seq_along(lvls), function(x) {
-      cbind(dat[var %in% lvls[seq(1, x)], ], frame = lvls[[x]])
-    })
-    dplyr::bind_rows(dats)
-  }
   
-  d <- modified_df %>%
-    filter(deadline >= 2009, success_rate) %>%
-    accumulate_by(~deadline)
-  
-  p2 <- d %>%
-    plot_ly(
-      x = ~deadline, 
-      y = ~success_rate,
-      frame = ~frame, 
-      type = 'scatter',
-      mode = 'lines', 
-      line = list(simplyfy = F, color = "rgb(202, 84, 133)")
-    ) %>% 
-  layout(
+  p2 <- plot_ly(
+    modified_df,
+    x = ~deadline,
+    y = ~success_rate,
+    type = "scatter",
+    mode = "lines+markers",
+    colors = "rgb(241, 171, 206)",
+    marker = list(color = "rgb(202, 84, 133)")
+  ) %>% 
+    layout(
+      autosize = T, margin = m,
+      title = paste("Success Rates in Years of", input_sub_cate),
+      font = list(color = "#C0C0C0"),
       xaxis = list(
         title = "Years",
-        color = "#C0C0C0",
+        color = "#C0C0C0", 
         zerolinewidth = 1,
         ticklen = 2,
         gridwidth = 1
       ),
       yaxis = list(
         title = "Success Rates (%)",
-        zerolinewidth = 1,
         color = "#C0C0C0",
         ticklen = 2,
         gridwidth = 1
       ),
       paper_bgcolor = "transparent",
       plot_bgcolor = "transparent"
-    ) %>% 
-    animation_opts(
-      frame = 100, 
-      transition = 0.5, 
-      redraw = FALSE
-    ) %>%
-    animation_slider(
-      hide = T
-    ) %>%
-    animation_button(
-      x = 1, xanchor = "right", y = 0, yanchor = "bottom"
     )
   
   return(p2)
