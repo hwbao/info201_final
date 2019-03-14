af94c88b802eeb165b0cf57902520878838ccf38
bravolistic <bravolistic16@gmail.com>
2019-03-14 00:41:37 -0700

    layout

--- total_backer.R
+++ total_backer.R
@@ -43,8 +43,7 @@ draw_sankey_graph <- function(kickstarter) {
   draw_graph <- plot_ly(type = "sankey", 
                         domain = list(x =  c(0,1), y =  c(0,1)),
                         orientation = "h",
-                        width = 1080,
-                        height = 1200,
+                        
                         textfont = list(size = 16,
                                         color = "rgb(194, 202, 214)"),
                         
@@ -58,7 +57,8 @@ draw_sankey_graph <- function(kickstarter) {
                           pad = 10,
                           thickness = 15,
                           color = my_color[0:20],
-                          line = list(color = 'black', width = 0.5)
+                          line = list(color = 'black', width = 0.5),
+                          font = list(color = '#C0C0C0')
                         ),
                         
                         link = list(source = arrange_data$fac_main_cate,
@@ -69,15 +69,16 @@ draw_sankey_graph <- function(kickstarter) {
                                     color = my_color[0:20])
   ) %>%
     layout(
-      title = "Total Pledged(USD) Each Category Get and Distribution to Their Sub Category",
+      title = "Total Pledged(USD) Within Each Category and its top 3 Distribution to Sub Categories",
       font = list(
         size = 11,
         color = "#C0C0C0"
       ),
+      autosize = T,
       xaxis = list(showgrid = T, zeroline = T, showticklabels = F),
       yaxis = list(showgrid = F, zeroline = F, showticklabels = F),
-      plot_bgcolor = "black",
-      paper_bgcolor = "black"
+      plot_bgcolor = "transparent",
+      paper_bgcolor = "transparent"
     )
   return(draw_graph)
 }
